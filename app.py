87a88aa38daccea28b4e8c589d81519916881089
urish11 <117926571+urish11@users.noreply.github.com>
2025-05-15 23:40:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -752,7 +752,7 @@ if st.button("Process trends with Gemini?", key='gemini_button', disabled=(GEMIN
                 with st.spinner("🧠 Processing with Gemini... This might take a moment."):
                     with st.expander("Prompt:"):
                         st.text(gemini_prompt)
-                    gemini_res = gemini_text_lib(gemini_prompt,model ="gemini-2.5-flash-preview-04-17") # Use the dedicated function gemini-2.5-pro-exp-03-25
+                    gemini_res = gemini_text_lib(gemini_prompt,model ="gemini-2.5-pro-exp-03-25") # Use the dedicated function gemini-2.5-pro-exp-03-25
             
                 if gemini_res:
                     # st.text(gemini_res) 
