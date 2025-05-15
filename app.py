6b78a63a0c4a67b840787d926dc75edc8aed4d42
urish11 <117926571+urish11@users.noreply.github.com>
2025-05-15 17:13:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -904,7 +904,7 @@ if 'final_merged_df' in st.session_state :
                         content = get_html_content(row['max_url'])
                         # st.text(content)
                         prompt = """write as html using only  <a>, <p>, <h2>–<h4>, <li>, <ul>, <img>.\n
-                        only the article content no footers no images!! no images! no writer name!, no <div>!!! first element is ALWAYS <p>. return in language same as input . return json dict, 2 keys : 'title', 'html'  . \n example :{"title" : "Learn more about how veterans ...", 'html' :"full article w/o title with html tags..'}  no <div>\n\n""" + content
+                        only the article content no footers no images!! no images! no writer name!, no <div>!!! first element is ALWAYS <p>. dont mention the domain input domain names is the title or html , omit that!! return in language same as input . return json dict, 2 keys : 'title', 'html'  . \n example :{"title" : "Learn more about how veterans ...", 'html' :"full article w/o title with html tags..'}  no <div>\n\n""" + content
                         gemini_res =gemini_text_lib(prompt=prompt, model='gemini-2.5-pro-exp-03-25' ) # gemini-2.0-flash-exp
                         # st.text(gemini_res)
                         pure_html = gemini_res.replace("```html","").replace("```","").replace("```json","").replace("json","")
