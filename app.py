8185a19d14e146ae1a9945d7d2d038b24a90fb7e
urish11 <117926571+urish11@users.noreply.github.com>
2025-08-05 18:41:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,8 @@ st.set_page_config(layout="wide",page_title= "FB Scrape", page_icon="🚀")
 
 temp_st=st.secrets
 try:
-    st.secrets = dict(os.environ)
+    if 'IMGBB_API_KEY' in os.environ : 
+        st.secrets = dict(os.environ)
 except:st.secrets=temp_st
 
 
